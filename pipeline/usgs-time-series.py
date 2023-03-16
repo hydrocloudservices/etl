18e1ef165f878ebee09810d9cb00d040bb7ce268
sebastienlanglois <sebastien.langlois@polymtl.ca>
2023-03-16 08:39:35 -0400

    usgs

--- pipeline/usgs-time-series.py
+++ pipeline/usgs-time-series.py
@@ -23,12 +23,13 @@ from config import Config
 def download_date(site):
     df = None
     try:
-        print(site)
-        df = nwis.get_record(sites=site, service='dv',  parameterCd='00060', start='1900-01-01', end='2023-03-06')
+        df = nwis.get_record(sites=site, service='dv',  parameterCd='00060', start='1900-01-01', end='2023-03-15')
         df['00060_Mean'] = df['00060_Mean'].mask(df['00060_Mean'] < 0)
         df = df[['00060_Mean', '00060_Mean_cd']]
-        df.columns = ['value', 'flag']
+        df.columns = ['discharge', 'discharge_flag']
         df.index.names = ['time']
+        df['source'] = 'USGS'
+        df = df.set_index(['source'], append=True)
         
     except:
         pass
@@ -38,7 +39,7 @@ def download_date(site):
 def process(df, site):
     ds = None
     try:
-        ds = pd.DataFrame(index=pd.date_range('1900-01-01','2023-03-06')).to_xarray().rename({'index':'time'})
+        ds = pd.DataFrame(index=pd.date_range('1900-01-01','2023-03-15')).to_xarray().rename({'index':'time'})
         ds_add = df.to_xarray()
         ds_add['time'] = pd.to_datetime(ds_add['time'])
         ds_add = ds_add.expand_dims({'id': [site]})
@@ -54,23 +55,24 @@ def get_clim(gauge_id, latitude, longitude, poly, unique_id):
     
     try:
 
-
         obj = TimezoneFinder()
 
         timezone = obj.timezone_at(lng=longitude, lat=latitude)
         timezone
 
         query = {
-            "variables": {"era5_reanalysis_single_levels": ["t2m", "tp"]},
+            "datasets": {"era5_reanalysis_single_levels": {"variables": ["t2m", "tp"]},
+                         "era5_land_reanalysis": {"variables": ["t2m", "tp", "sd"]}},
             "space": {
                 "clip": "polygon", # bbox, point or polygon
-                "aggregation": True,
+                "averaging": True,
                 "geometry": poly,
                 "unique_id": unique_id
             },
             "time": {
                 "timestep": "D", # http://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#offset-aliases
-                "aggregation": {"tp": [np.nansum],
+                "aggregation": {"sd": [np.nanmean],
+                                "tp": [np.nansum],
                                 "t2m": [np.nanmax, np.nanmean, np.nanmin]},
 
                 "start": '1978-12-31',
@@ -78,7 +80,7 @@ def get_clim(gauge_id, latitude, longitude, poly, unique_id):
                 "timezone": timezone,
             },
         }
-        data = xd.Dataset(**query).data
+        data = xd.Query(**query).data
     except:
         pass
     return data
@@ -86,7 +88,9 @@ def get_clim(gauge_id, latitude, longitude, poly, unique_id):
 def merge_datasets(ds, ds_clim):
     ds_combined = None
     try:
-        ds_clim = ds_clim.expand_dims('gauge_id').rename({'gauge_id': 'id'}).drop(['latitude','longitude','id'])
+        print(ds_clim)
+        print(ds)
+        ds_clim = ds_clim.drop(['latitude','longitude','id'])
         ds_combined = xr.merge([ds, ds_clim])
     except:
         pass
@@ -111,7 +115,7 @@ def process_one_file(row):
 
     df = download_date(site)
     ds = process(df, site)
-    ds_clim = get_clim(f"hysets_{site}", latitude, longitude, poly, "gauge_id")
+    ds_clim = get_clim(site, latitude, longitude, poly, "id")
     ds_merged = merge_datasets(ds, ds_clim)
     save_zarr(ds_merged, site)
 
@@ -119,12 +123,16 @@ def process_one_file(row):
 if __name__ == '__main__':
 
     # Move hard-coded path to Config.py
-    gdf = dask_geopandas.read_parquet('s3://hydrometric/shapefiles/geoparquet', 
+    gdf = dask_geopandas.read_parquet('s3://hydrometric/watershed/combined_spatial.parquet', 
                                       storage_options=Config.STORAGE_OPTIONS)
 
-    # Will use a pilot DB instead
-    df_stations = pd.read_csv('data/attributes_other_hysets.csv')
-    df_stations['number'] = df_stations.apply(lambda row: row.gauge_id.split('_')[-1], axis=1)
+    #gdf = gdf.loc[gdf.source.isin(['USGS'])]
+
+    gdf = gdf.loc[gdf.source.isin(['USGS'])].compute()
+    gdf = gdf.reset_index(drop=True)
+    # # Will use a pilot DB instead
+    # df_stations = pd.read_csv('data/attributes_other_hysets.csv')
+    # df_stations['number'] = df_stations.apply(lambda row: row.gauge_id.split('_')[-1], axis=1)
 
     # Function to verify if file exists and if has been processed in the last 7 days
     # If not, then process then file
@@ -136,14 +144,18 @@ if __name__ == '__main__':
     ]
 
     iterable = []
-    for idx, row in df_stations.iterrows():
-        site = row.number
-        latitude = row.gauge_lat
-        longitude = row.gauge_lon
-        if site not in files_already_processed:
-            poly = gdf[gdf.gauge_id == f"hysets_{site}"].reset_index(drop=True).compute()
-            iterable.append([site, latitude, longitude, poly])
+    for idx, row in gdf.iterrows():
+        latitude = None
+        longitude = None
+        site = row.id
 
+        oneSite = nwis.what_sites(sites=site)
+        latitude = oneSite[0].dec_lat_va.values[0]
+        longitude = oneSite[0].dec_long_va.values[0]
+        print(f"{idx}: {site}")
+        if site not in files_already_processed:
+            poly = gdf.loc[gdf.id == site]
+            iterable.append([site, latitude, longitude, poly])
 
 
     with Flow("USGS-time-series") as flow:
