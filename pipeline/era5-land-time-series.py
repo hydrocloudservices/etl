0132956aa1dee4bb1cbfe9fcc8264b0d62449a36
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2025-02-12 11:15:59 -0500

    Update era5-land-time-series.py

--- pipeline/era5-land-time-series.py
+++ pipeline/era5-land-time-series.py
@@ -54,6 +54,8 @@ def get_file_pattern():
         end_date = pd.date_range(Config.E5L_START_DATE_TS, periods=delta_days)[-1].strftime('%Y%m%d')
         pass
 
+    end_date = '20240401'
+
     years = pd.date_range(Config.E5L_START_DATE_TS, end_date)
 
     pattern = FilePattern(
